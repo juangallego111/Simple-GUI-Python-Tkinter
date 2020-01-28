961fd2fe7fac92c4791aefe433abb4125fd4def3
Juan Gallego <jgg00083@red.ujaen.es>
2020-01-28 16:14:51 +0100

    Aprendizaje: Eventos con botones y clases

---
+++ aprendizaje.py
@@ -0,0 +1,86 @@
+from tkinter import *
+
+#root = Tk()
+"""topFrame = Frame(root)
+topFrame.pack()
+
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+button1 = Button(topFrame, text= "Button 1", fg="blue")
+button2 = Button(topFrame, text= "Button 2", fg="yellow")
+button3 = Button(topFrame, text= "Button 3", fg="red")
+button4 = Button(bottomFrame, text= "Button 4", fg="green")
+
+button1.pack(side=LEFT)
+button2.pack(side=LEFT)
+button3.pack(side=LEFT)
+button4.pack() 
+thelabel2 = Label( root, text="Texto expandido X",  bg="black", fg="white")
+thelabel2.pack(fill=X)
+
+thelabel3 = Label( root, text="Texto expandido Y",  bg="black", fg="white")
+thelabel3.pack(fill=Y, side= BOTTOM)
+"""
+#Entradas y Grid
+"""
+user = Label(root, text="Usuario")
+password = Label(root, text="Password")
+entrada1 = Entry(root)
+entrada2 = Entry(root)
+
+user.grid(row=0, sticky=E)
+password.grid(row=1, sticky=E)
+entrada1.grid(row=0, column=1)
+entrada2.grid(row=1, column=1,columnspan=2)
+
+    #Checkbutton
+
+check= Checkbutton(root, text="Keep me logged in")
+check.grid(columnspan=2)
+"""
+#Click Button y evento
+"""
+def printName(event):
+    print("Mi nombre es Juan")
+
+#botonPrint = Button(root, text="¿Como te llamas?", command=printName)
+botonPrint = Button(root, text="¿Como te llamas?")
+#Eventos
+botonPrint.bind("<Button-1>", printName)
+botonPrint.pack()
+"""
+# Eventos en distintos clicks en frames
+"""
+def leftClick(event):
+    print("Izquierda")
+
+def rigthClick(event):
+    print("Derecha")
+
+def middleClick(event):
+    print("Medio")
+
+frame = Frame(root, width=300, height=250)
+frame.bind("<Button-1>",leftClick)
+frame.bind("<Button-3>",rigthClick)
+frame.bind("<Button-2>",middleClick)
+frame.pack()
+"""
+
+# Classes
+class ButtonsClass:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.printButton = Button(frame, text="Print", command=self.printMessage)
+        self.printButton.pack(side=LEFT)
+
+        self.quickButton = Button(frame, text="Exit", command=frame.quit)
+        self.quickButton.pack(side=LEFT)
+    
+    def printMessage(self):
+        print("Lesson 8")
+root= Tk()
+b = ButtonsClass(root)
+root.mainloop()
