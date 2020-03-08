91139b9629e9ac45e992f78abc864ed92e8754b9
Juan Gallego <jgg00083@red.ujaen.es>
2020-03-08 16:32:12 +0100

    InterfazCoches:Mejora de Tesseract

--- Interfaz_Coches.py
+++ Interfaz_Coches.py
@@ -12,10 +12,10 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 
 
 
-CONF_THRESH, NMS_THRESH = 0.5, 0.5
+CONF_THRESH, NMS_THRESH = 0.2, 0.2
 
 CONFIG = "C:/Users/usuario/Desktop/darknet/yolov3.cfg" #'darknet/yolov31.cfg'
-WEIGHTS = "C:/Users/usuario/Desktop/darknet/backup/yolov3_last_final.weights" #'darknet/backup/yolov3_2000.weights'
+WEIGHTS = "C:/Users/usuario/Desktop/darknet/backup/yolov3_2000.weights" #'darknet/backup/yolov3_2000.weights'
 NAMES = "C:/Users/usuario/Desktop/darknet/obj.names" #'darknet/obj.names'
 
 def canvasInterfaceframe(frame):
@@ -23,14 +23,15 @@ def canvasInterfaceframe(frame):
     canvasCoches.grid(row=0, column=0)
     label1 = tkinter.Label(frame, text = "Elige la Matrícula")
     label1.grid(row=0, column=1, padx=10)
-    combo = ttk.Combobox(frame,state="readonly")
-    combo.grid(row=0, column=2, padx=10)
-    combo["values"] = ["Python", "C", "C++", "Java"]
+    lista = tkinter.Listbox(frame)
+    lista.grid(row=0, column=2, padx=10)
+    values= ("Python", "C", "C++", "Java")
+    lista.insert(0,*values)
     #Llama a la funcion cuando cambian de selección
     #combo.bind("<<ComboboxSelected>>", self.selection_changed)
     insertButton = tkinter.Button(frame, text = "Insertar", command = lambda : abrirArchivo(canvasCoches))
     insertButton.grid(row=1, column=0, pady=10)
-    runButton = tkinter.Button(frame, text = "Ejecutar", command = lambda : ejecutar_red(ruta_ima, canvasCoches, combo))
+    runButton = tkinter.Button(frame, text = "Ejecutar", command = lambda : ejecutar_red(ruta_ima, canvasCoches, lista))
     runButton.grid(row=1, column=1, pady=10)
     #insertButton.bind("<Button-1>", abrirArchivo)
     
@@ -99,8 +100,9 @@ def pulsaFotos():
 def cerrar_app():
     ventanaPrincipal.destroy()
 
-def ejecutar_red(imagen_red, canvasCoches,combo):
+def ejecutar_red(imagen_red, canvasCoches,lista):
     # Load the network
+    lista.delete(0, END)
     net = cv2.dnn.readNetFromDarknet(CONFIG, WEIGHTS)
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
@@ -111,8 +113,8 @@ def ejecutar_red(imagen_red, canvasCoches,combo):
 
     # Read and convert the image to blob and perform forward pass to get the bounding boxes with their confidence scores
     img = cv2.imread(imagen_red)
-    height, width = img.shape[:2]
-
+    img_bn = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    height, width = img_bn.shape[:2]
     blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), swapRB=True, crop=False)
     net.setInput(blob)
     layer_outputs = net.forward(output_layers)
@@ -141,32 +143,33 @@ def ejecutar_red(imagen_red, canvasCoches,combo):
     with open(NAMES, "r") as f:
         classes = [line.strip() for line in f.readlines()]
     colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    color = (255, 0, 0) 
     for index in indices:
         x, y, w, h = b_boxes[index]
         print(x,y,w,h)
-        crop_img = img[y:y+h, x:x+w]
-        texto = pytesseract.image_to_string(crop_img, config='--psm 11')
+        crop_img = img_bn[y:y+h, x:x+w]
+
+        ret,thresh = cv2.threshold(crop_img,0,255,cv2.THRESH_BINARY | cv2.THRESH_TRIANGLE)
+        #ret,thresh2 = cv2.threshold(crop_img,127,255,cv2.THRESH_BINARY)
+        #contornos, jerarquia = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        cv2.imwrite("1_1"+".jpg",thresh)
+        blurred = cv2.GaussianBlur(thresh, (3, 3), 0)
+        #wide = cv2.Canny(crop_img, 10, 200)
+        #edged = cv2.Canny(crop_img, lower, upper)
+        texto = pytesseract.image_to_string(blurred, config='-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 --psm 7') #config='--psm 7'
+        #texto1 = pytesseract.image_to_string(crop_img, config='--psm 7')
+        #texto1 = pytesseract.image_to_string(blurred, lang='eng', config='-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+        #cv2.imwrite("recortada"+".jpg",crop_img)
+
         print(texto)
-        values = list(combo["values"])
-        combo["values"] = values + [texto]
-        cv2.rectangle(img, (x, y), (x + w, y + h), colors[0,index], 2)
+        lista.insert(lista.size(), texto)
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
     
     global nombre_img
     nombre_img = nombre_img + 1
     cv2.imwrite("resultado"+str(nombre_img)+".jpg",img)
     mostrar_imagen(canvasCoches)
     #leer_matricula(x,y,w,h,imagen_red)
-    
-def leer_matricula(x,y,w,h,img):
-    x = int(x)
-    y = int(y)
-    w = int(w)
-    h = int(h)
-    print(type(x))
-    # Leemos las matriculas
-    crop_img = img[y:y+h, x:x+w]
-    texto = pytesseract.image_to_string(crop_img, config='--psm 11')
-    print(texto)
 
 #MAIN
 nombre_img = 0
